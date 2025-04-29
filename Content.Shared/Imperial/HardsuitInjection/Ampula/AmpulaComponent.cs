09cbd6ba98737d988696ac87a9050f1e3626a560
Siginanto <fantik8800@gmail.com>
2025-04-29 15:44:50 +1000

    Ампулы и их механ

---
+++ Content.Shared/Imperial/HardsuitInjection/Ampula/AmpulaComponent.cs
@@ -0,0 +1,10 @@
+using Content.Shared.Clothing.EntitySystems;
+using Content.Shared.Imperial.HardsuitInjection.EntitySystems;
+
+namespace Content.Shared.Imperial.HardsuitInjection.Components;
+
+[Access(typeof(AmpulaSystem))]
+[RegisterComponent]
+public sealed partial class AmpulaComponent : Component
+{
+}
